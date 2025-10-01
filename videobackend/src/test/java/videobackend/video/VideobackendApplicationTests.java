ea76bf0c8b498e936d4cf812ac20f7e2d2522be2
HSJ <2641065789@qq.com>
2025-10-02 01:22:29 +0800

    创建后端

---
+++ videobackend/src/test/java/videobackend/video/VideobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package videobackend.video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideobackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
